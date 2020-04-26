c75385836b56fb9501fb4dab083a58ff81252915
rikkebm <60385097+rikkebm@users.noreply.github.com>
2020-04-26 15:11:02 +0200

    Update question2.js

--- question2.js
+++ question2.js
@@ -1,1 +1,36 @@
+const gamesUrl = "https://api.rawg.io/api/games";
 
+async function fetchImage() {
+  try {
+    const response = await fetch(gamesUrl);
+    const gamesObject = await response.json();
+    createGames(gamesObject);
+  } catch (error) {
+    console.log(error);
+  }
+}
+fetchImage();
+
+function createGames(gamesObject) {
+  console.dir(gamesObject);
+
+  const resultContainer = document.querySelector(".results");
+
+  let html = "";
+
+  for (let i = 0; i < gamesObject.length; i++) {
+    if (!gamesObject[i].results) {
+      continue;
+    }
+
+    console.dir(gamesObject[i].results);
+
+    html += `<div>
+                    <div class="game">
+                    <h2>${gamesObject[i].name}</h2>
+                    <img src="url(${gamesObject[i].background_image});" alt="${gamesObject[i].name}" /></div>
+                </div>`;
+  }
+
+  results.innerHTML = html;
+};
