c9e81fa80dc6fe9fd54c9e6c84a7c380d3310ba4
rikkebm <60385097+rikkebm@users.noreply.github.com>
2020-04-26 15:08:49 +0200

    Update question1.js

--- question1.js
+++ question1.js
@@ -1,1 +1,15 @@
+const firstName = document.querySelector("#firstName");
+const firstNameError = document.querySelector("#firstNameError");
 
+firstName.addEventListener("submit", checkLength);
+
+function checkLength(event) {
+    const inputValue = event.target.value;
+    const valueLength = inputValue.length;
+
+    if (valueLength >= 2) {
+        firstNameError.style.display = "none";
+    } else {
+        firstNameError.style.display = "block";
+    }
+}
